ccd8f800d8f9c742375324210f758db004a4a739
Bhavinkumar Patel <patelbhavin27@gmail.com>
2014-12-31 14:27:07 -0800

    Initial Commit

---
+++ tipcal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcal
+//
+//  Created by Bhavinkumar Patel on 12/31/14.
+//  Copyright (c) 2014 Bhavinkumar Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcal
+//
+//  Created by Bhavinkumar Patel on 12/31/14.
+//  Copyright (c) 2014 Bhavinkumar Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
